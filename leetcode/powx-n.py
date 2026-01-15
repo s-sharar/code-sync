65c701f26c521723bcb453b5f3ca011459f271e2
Shihan Sharar <ssharar@uwaterloo.ca>
2026-01-15 16:15:11 -0500

    Add solution for powx-n

---
+++ leetcode/powx-n.py
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        long long e = n;
+        if (e < 0) { x = 1.0 / x; e = -e; }
+
+        double ans = 1.0;
+        while (e > 0) {
+            if (e & 1) ans *= x;
+            x *= x;
+            e >>= 1;
+        }
+        return ans;
+    }
+};
