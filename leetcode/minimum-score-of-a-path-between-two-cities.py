1c7121edba2b2c7b6d12d43030a6c3d1ebc915e3
Shihan Sharar <ssharar@uwaterloo.ca>
2026-07-04 17:17:29 -0700

    Update solution for minimum-score-of-a-path-between-two-cities

--- leetcode/minimum-score-of-a-path-between-two-cities.py
+++ leetcode/minimum-score-of-a-path-between-two-cities.py
@@ -1,6 +1,35 @@
 class Solution {
 public:
     int minScore(int n, vector<vector<int>>& roads) {
-        
+        vector<vector<pair<int, int>>> adj(n + 1);
+
+        for (auto& r : roads) {
+            int a = r[0], b = r[1], d = r[2];
+            adj[a].push_back({b, d});
+            adj[b].push_back({a, d});
+        }
+
+        vector<int> vis(n + 1, 0);
+        queue<int> q;
+        q.push(1);
+        vis[1] = 1;
+
+        int ans = INT_MAX;
+
+        while (!q.empty()) {
+            int node = q.front();
+            q.pop();
+
+            for (auto [nei, dist] : adj[node]) {
+                ans = min(ans, dist);
+
+                if (!vis[nei]) {
+                    vis[nei] = 1;
+                    q.push(nei);
+                }
+            }
+        }
+
+        return ans;
     }
 };
